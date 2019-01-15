9f82a9cfa583bfb5ea63e64a4f8721b565a4617d
codpls <3937605+codpls@users.noreply.github.com>
2019-01-16 10:44:36 +1100

    combine version 2 & 3

--- host/jupyter-chrome-starter.bat
+++ host/jupyter-chrome-starter.bat
@@ -3,4 +3,4 @@
 :: Use of this source code is governed by a BSD-style license that can be
 :: found in the LICENSE file.
 
-python "%~dp0/jupyter-chrome-starter-host-py3" %*
+python "%~dp0/jupyter-chrome-starter-host" %*
